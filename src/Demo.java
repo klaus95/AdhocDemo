72973b70840229e48ac6a39931865761636dd82f
klaus95 <kcipi2015@my.fit.edu>
2019-03-03 05:38:26 -0500

    Testing

--- src/Demo.java
+++ src/Demo.java
@@ -30,6 +30,7 @@ public class Demo {
                 //Create the client side
                 //Send info from textField1-4
                 //Create the new window and close the old one
+                //Change
             }
         });
     }
